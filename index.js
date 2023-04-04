bfff8000e7f28136fea6aa7ac0c17ee0d1d87c9c
Ayush Chothe <ayushmchothe@gmail.com>
2023-04-05 00:15:37 +0530

    7

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.use(express.json());
 
 const TOKEN = "bot5896325394:AAFavOTsIbpsocGE6ep1zvOEbTyRnV7bOJU";
 const bot = new TelegramBot(TOKEN);
-bot.setWebHook("https://telebot-production-29ce.up.railway.app/");
+// bot.setWebHook("https://telebot-production-29ce.up.railway.app/");
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   let {
     message: {
       from: { first_name },
@@ -20,13 +20,13 @@ app.post("/", (req, res) => {
 
   switch (text) {
     case "Hi":
-      bot.sendMessage(chatId, `Hello ${first_name}`);
+      await bot.sendMessage(chatId, `Hello ${first_name}`);
       break;
     case "Ayush":
-      bot.sendMessage(chatId, "Hey Ash");
+      await bot.sendMessage(chatId, "Hey Ash");
       break;
     case "Chetan":
-      bot.sendMessage(chatId, "Hi Storm");
+      await bot.sendMessage(chatId, "Hi Storm");
       break;
   }
 
